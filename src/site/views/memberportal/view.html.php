4698937c63366fec6b25eca66ae4032475ce3238
Eric Wong <slbteam08@hotmail.com>
2023-04-14 01:16:49 +0800

    Update cell group event type value

--- src/site/views/memberportal/view.html.php
+++ src/site/views/memberportal/view.html.php
@@ -89,7 +89,7 @@ class MemberPortalViewMemberPortal extends JViewLegacy
 			}
 		}
 		foreach($this->attd_cell_dates as $date) {
-			if ($date->event_type == "小組") {
+			if ($date->event_type == "小組聚會") {
 				$present = $cell_present;
 			} else {
 				$present = $zone_present;
